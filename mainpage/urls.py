d6f041bb46fe61cba29f0cbdebcd61b7e9c51833
extroed <extroed@gmail.com>
2024-10-29 10:56:55 +0300

    changed urls with router, updated visual

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,24 +1,27 @@
-from django.urls import path
-from .views import main_banner
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import (
-    PersonalInfoListCreateAPIView,
-    AdditionalInfoListCreateAPIView,
-    ExperienceListCreateAPIView,
-    WorkExperienceListCreateAPIView,
-    PortfolioListCreateAPIView,
-    ContactMeAPIView,
+    PersonalInfoViewSet,
+    AdditionalInfoViewSet,
+    ExperienceViewSet,
+    WorkExperienceViewSet,
+    PortfolioViewSet,
+    ContactMeAPIView, main_banner,
 )
 from . import views
 
 
+router = DefaultRouter()
+router.register(r'personal-info', PersonalInfoViewSet)
+router.register(r'additional-info', AdditionalInfoViewSet)
+router.register(r'experience', ExperienceViewSet)
+router.register(r'work-experience', WorkExperienceViewSet)
+router.register(r'portfolio', PortfolioViewSet)
+router.register(r'contact-me', ContactMeAPIView)
+
 urlpatterns = [
-    path("", main_banner, name="main-banner"),
-    path("personal-info/", PersonalInfoListCreateAPIView.as_view(), name="personal-info"),
-    path("additional-info/", AdditionalInfoListCreateAPIView.as_view(), name="additional-info"),
-    path("experience/", ExperienceListCreateAPIView.as_view(), name="experience"),
-    path("work-experience/", WorkExperienceListCreateAPIView.as_view(), name="work-experience"),
-    path("portfolio/", PortfolioListCreateAPIView.as_view(), name="portfolio"),
-    path("contact-me/", ContactMeAPIView.as_view(), name="contact-me"),
+    path('', main_banner, name='main-banner'),
+    path('api/', include(router.urls)),  # Добавляем маршруты из роутера
     path('test-email/', views.test_view, name='test-email'),
-    path("send-email/", views.send_email_view, name="send-email"),
+    path('send-email/', views.send_email_view, name='send-email'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework import viewsets
 from .models import PersonalInfo, AdditionalInfo, Experience, WorkExperience, Portfolio, ContactMessage
 from .serializers import (
     PersonalInfoSerializer,
@@ -28,32 +28,32 @@ def main_banner(request):
     return render(request, "base.html", context)
 
 
-class PersonalInfoListCreateAPIView(ListAPIView):
+class PersonalInfoViewSet(viewsets.ModelViewSet):
     queryset = PersonalInfo.objects.all()
     serializer_class = PersonalInfoSerializer
 
 
-class AdditionalInfoListCreateAPIView(ListAPIView):
+class AdditionalInfoViewSet(viewsets.ModelViewSet):
     queryset = AdditionalInfo.objects.all()
     serializer_class = AdditionalInfoSerializer
 
 
-class ExperienceListCreateAPIView(ListAPIView):
+class ExperienceViewSet(viewsets.ModelViewSet):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
 
 
-class WorkExperienceListCreateAPIView(ListAPIView):
+class WorkExperienceViewSet(viewsets.ModelViewSet):
     queryset = WorkExperience.objects.all()
     serializer_class = WorkExperienceSerializer
 
 
-class PortfolioListCreateAPIView(ListAPIView):
+class PortfolioViewSet(viewsets.ModelViewSet):
     queryset = Portfolio.objects.all()
     serializer_class = PortfolioSerializer
 
 
-class ContactMeAPIView(CreateAPIView):
+class ContactMeAPIView(viewsets.ModelViewSet):
     queryset = ContactMessage.objects.all()
     serializer_class = ContactMessageSerializer
 
@@ -66,20 +66,18 @@ def test_view(request):
 @csrf_exempt
 def send_email_view(request):
     if request.method == "POST":
-        data = json.loads(request.body)  # Получаем данные из POST-запроса
+        data = json.loads(request.body)
         name = data.get("name")
         email = data.get("email")
         message = data.get("message")
 
-        # Сформируйте сообщение для отправки
         full_message = f"Message from {name} ({email}):\n\n{message}"
 
-        # Отправьте email
         send_mail(
             subject="New Contact Message",
             message=full_message,
-            from_email="ExtroEdLive@gmail.com",  # Ваш email-отправитель
-            recipient_list=["ExtroEdLive@gmail.com"],  # Ваш email-получатель
+            from_email="ExtroEdLive@gmail.com",
+            recipient_list=["ExtroEdLive@gmail.com"],
             fail_silently=False,
         )
         return JsonResponse({"status": "Email sent successfully"})
